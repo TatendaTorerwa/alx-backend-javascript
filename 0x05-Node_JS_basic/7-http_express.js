6881afa628f4f0d2c936a7b7f66f8712782d217b
TatendaTorerwa <ttorerwa01@gmail.com>
2024-06-18 18:11:32 +0000

    7. Create a more complex HTTP server using Express.

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,35 +1,63 @@
 const express = require('express');
-const fs = require('fs');
-const countStudents = require('./3-read_file_async'); // Assuming 3-read_file_async.js is correctly implemented
+
+const { readFile } = require('fs');
 
 const app = express();
 const port = 1245;
-const databasePath = process.argv[2]; // Database file path passed as argument
 
-// Route for /
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
 app.get('/', (req, res) => {
-  res.type('text/plain');
   res.send('Hello Holberton School!');
 });
-
-// Route for /students
 app.get('/students', (req, res) => {
-  res.type('text/plain');
-  res.send('This is the list of our students\n');
-
-  // Call countStudents function from 3-read_file_async.js
-  countStudents(databasePath)
-    .then(() => {
-      console.log('Done reading students data');
-    })
-    .catch((error) => {
-      console.error('Error reading students data:', error.message);
-    });
+  countStudents(process.argv[2].toString()).then((output) => {
+    res.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    res.send('This is the list of our students\nCannot load the database');
+  });
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running and listening on http://localhost:${port}`);
 });
 
 module.exports = app;
